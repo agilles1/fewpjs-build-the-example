197daa69d32321140431399b46035a868bf63443
agilles1 <adam.g.tpt@gmail.com>
2021-02-15 21:33:59 -0500

    Done.

--- main.js
+++ main.js
@@ -1,50 +1,71 @@
-// Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
-const likeButtons = document.getElementsByClassName("like")
+const likeButtons = document.querySelectorAll(".like")
 const error = document.getElementById("modal")
 error.setAttribute("class", "hidden")
 
+let glyphStates = {
+  "♡": "♥",
+  "♥": "♡"
+};
 
-let i= 0 
-for (i; i < likeButtons.length; i++) {
-  likeButtons[i].addEventListener("click", function(e){
-    mimicServerCall()
-    .then(likePost(e))
-    .catch(() =>{
-      error.setAttribute("class", "show")
-      setTimeout(() => {error.setAttribute("class", "hidden")}, 5000)
-    })
+let colorStates = {
+  "activated-heart" : "like-glyph",
+  "like-glyph": "activated-heart"
+};
+
+
+
+// let i= 0 
+// for (i; i < likeButtons.length; i++) {
+//   likeButtons[i].addEventListener("click", function(e){
+//     mimicServerCall()
+//     .then(likePost(e))
+//     .catch(() =>{
+//       console.log("your in first")
+//       error.setAttribute("class", "show")
+//       setTimeout(() => {error.setAttribute("class", "hidden")}, 5000)
+//     })
  
-  })}
+//   })}
 
   function likePost(e){
-    let heart = e.target.parentElement
-    heart.innerHTML = `<li>Like! <span class="like-glyph">${FULL_HEART}</span></li>`
-    heart.firstElementChild.setAttribute("class", "activated-heart")
-    heart.addEventListener("click", function(evt){
-      mimicServerCall()
-      .then(unlikePost(evt))
-      .catch(() =>{
-        error.setAttribute("class", "show")
-        setTimeout(() => {error.setAttribute("class", "hidden")}, 5000)
-      })
+    let heart = e.target;
+    mimicServerCall()
+    .then(function(){
+       heart.innerText = glyphStates[heart.innerText];
+       heart.setAttribute("class", colorStates[heart.classList.value])
+       debugger
     })
+    .catch(function() {
+      error.setAttribute("class", "show")
+      setTimeout(() => {error.setAttribute("class", "hidden")}, 5000)
+      })
   }
 
-  function unlikePost(e){
-    let heart = e.target
-    heart.innerHTML = `Like! <span class="like-glyph">${EMPTY_HEART}</span>`
-    heart.setAttribute("class", "like")
-    heart.addEventListener("click", function(evt){
-      mimicServerCall()
-      .then(likePost(evt))
-      .catch(() =>{
-        error.setAttribute("class", "show")
-        setTimeout(() => {error.setAttribute("class", "hidden")}, 5000)
-      })
-    })
+  // function unlikePost(e){
+  //   let heart = e.target
+  //   heart.innerHTML = `Like! <span class="like-glyph">${EMPTY_HEART}</span>`
+  //   heart.setAttribute("class", "like")
+  //   heart.addEventListener("click", function(evt){
+  //     mimicServerCall()
+  //     .then(likePost(evt))
+  //     .catch(() =>{
+  //       console.log("your in unlike")
+  //       error.setAttribute("class", "show")
+  //       setTimeout(() => {error.setAttribute("class", "hidden")}, 5000)
+  //     })
+  //   })
+  // }
+
+  // likeButtons.forEach(glyph => {
+  //   glyph.addEventListener("click", likePost)
+  // })
+
+  for (let glyph of likeButtons) {
+    glyph.addEventListener("click", likePost);
   }
+
 
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
