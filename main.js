5d66aa4f3e15ff0ce8b222c95de8a55d9a6ec9b3
agilles1 <adam.g.tpt@gmail.com>
2021-02-15 20:14:21 -0500

    Done.

--- main.js
+++ main.js
@@ -1,15 +1,50 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const likeButtons = document.getElementsByClassName("like")
+const error = document.getElementById("modal")
+error.setAttribute("class", "hidden")
 
-// Your JavaScript code goes here!
 
+let i= 0 
+for (i; i < likeButtons.length; i++) {
+  likeButtons[i].addEventListener("click", function(e){
+    mimicServerCall()
+    .then(likePost(e))
+    .catch(() =>{
+      error.setAttribute("class", "show")
+      setTimeout(() => {error.setAttribute("class", "hidden")}, 5000)
+    })
+ 
+  })}
 
+  function likePost(e){
+    let heart = e.target.parentElement
+    heart.innerHTML = `<li>Like! <span class="like-glyph">${FULL_HEART}</span></li>`
+    heart.firstElementChild.setAttribute("class", "activated-heart")
+    heart.addEventListener("click", function(evt){
+      mimicServerCall()
+      .then(unlikePost(evt))
+      .catch(() =>{
+        error.setAttribute("class", "show")
+        setTimeout(() => {error.setAttribute("class", "hidden")}, 5000)
+      })
+    })
+  }
 
-
-//------------------------------------------------------------------------------
-// Ignore after this point. Used only for demo purposes
-//------------------------------------------------------------------------------
+  function unlikePost(e){
+    let heart = e.target
+    heart.innerHTML = `Like! <span class="like-glyph">${EMPTY_HEART}</span>`
+    heart.setAttribute("class", "like")
+    heart.addEventListener("click", function(evt){
+      mimicServerCall()
+      .then(likePost(evt))
+      .catch(() =>{
+        error.setAttribute("class", "show")
+        setTimeout(() => {error.setAttribute("class", "hidden")}, 5000)
+      })
+    })
+  }
 
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
